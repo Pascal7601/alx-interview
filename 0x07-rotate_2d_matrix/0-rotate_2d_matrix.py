de43aa288ab872f7bf9312f8258d211b76604ad7
Pascal7601 <levispascal63@gmail.com>
2024-09-11 11:44:21 -0700

    rotate a matrix

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,40 +1,33 @@
 #!/usr/bin/python3
 """
-rotating a 2dimensional matrix in its place
+Rotating a 2-dimensional matrix in its place
 """
+
+
 def rotate_2d_matrix(matrix):
-  """
-  rotates a 2d matrix in place
-  """
-  # [[4, 3, 1],
-   # [2, 5, 7],
-   # [6, 9, 8]]
-  left, right = 0, len(matrix) - 1 # a left varibale to represent the left index values
-  #and right for right index values
-  top, bottom = left, right
+    """
+    Rotates a 2D matrix 90 degrees clockwise in place
+    """
+    left, right = 0, len(matrix) - 1
 
-  while left < right:
-    for i in range(right - left): # n * n matrix, = n -1 circles
-      # temporary store the top most left value
-      top_left = matrix[top][left + i]
+    while left < right:
+        for i in range(right - left):
+            top, bottom = left, right
 
-      # move the bottom left value to the top left
-      matrix[top][left + i] = matrix[bottom][left + i]
+            # Save the top left value
+            top_left = matrix[top][left + i]
 
-      # move the bottom right value to the bottom left
-      matrix[bottom][right - i] = matrix[bottom][left]
+            # Move bottom left to top left
+            matrix[top][left + i] = matrix[bottom - i][left]
 
-      # move the top right value to the bottom right
-      matrix[top][right - i] = matrix[bottom][right - i]
+            # Move bottom right to bottom left
+            matrix[bottom - i][left] = matrix[bottom][right - i]
 
-      # move the value in the temp variable to the top right
-      matrix[top][right - i] = top_left
+            # Move top right to bottom right
+            matrix[bottom][right - i] = matrix[top + i][right]
 
-matrix = [[1, 2, 3],
-          [4, 5, 6],
-          [7, 8, 9]]
-rotate_2d_matrix(matrix)
-print(matrix)
+            # Move top left (saved) to top right
+            matrix[top + i][right] = top_left
 
-
-
+        left += 1
+        right -= 1
